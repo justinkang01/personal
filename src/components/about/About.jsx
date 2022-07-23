ba724be829d7c25eddbd5981396ecdc656b0add0
Justin Kang <68215015+justinkang01@users.noreply.github.com>
2022-07-23 13:28:21 -0700

    temp disable mobile viewing, add images

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import classnames from 'classnames/bind';
-import getWindowDimensions from 'components/utils';
+import { getWindowDimensions, useImage, useWindowDimensions } from 'components/utils';
 // import logo from 'logo.svg';
 import data from './data';
 import styles from './about.module.scss';
@@ -24,11 +24,18 @@ const Bubble = ({ size, data, index }) => {
         height: `${size}px`,
         width: '50%',
     };
+    const { loading, error, image } = useImage(imgSrc);
+
+    useEffect(() => {
+        if (error) {
+            return <div>{error}</div>;
+        }
+    }, [error]);
 
     return (
         <div className={styles['bubbleContainer']}>
             <div style={style} className={classNameBuilder(styles.bubble, { evenRow: index % 2 })}>
-                <img alt={alt} src={imgSrc} />
+                {loading ? <div className={styles.loader}>loading image...</div> : <img alt={alt} src={image} />}
             </div>
             <div style={textStyle} className={styles.bubbleText}>
                 {text}
@@ -37,25 +44,13 @@ const Bubble = ({ size, data, index }) => {
     );
 };
 
-const initWindowDimensions = getWindowDimensions();
-
 /**
  * @returns The About page
  */
 const About = () => {
-    const [windowSize, setWindowSize] = useState(initWindowDimensions);
+    const windowSize = useWindowDimensions();
     const size = Math.min(windowSize.width, windowSize.height) / data.length;
     const [bubbleSize, setBubbleSize] = useState(size);
-
-    const resizeCallback = useCallback(() => {
-        setWindowSize(getWindowDimensions());
-    }, [setWindowSize]);
-
-    // checks to see if the window is being resized and sets the window state
-    useEffect(() => {
-        window.addEventListener('resize', resizeCallback);
-        return () => window.removeEventListener('resize', resizeCallback);
-    }, [resizeCallback]);
 
     // theoretically, whenever the windowSize changes, this should change the size of the bubbles to match
     useLayoutEffect(() => {
@@ -64,7 +59,10 @@ const About = () => {
     }, [windowSize]);
 
     /**
-     * @todo change the styling of a bubble based on flex direction
+     * @todo currently, statically render in bubbles based on data.
+     * in future, want to update the look of the site to render in bubbles
+     * and onhover and onclick changes the about section.
+     *
      */
     return (
         <div className={styles.aboutContainer}>
--- src/components/utils.jsx
+++ src/components/utils.jsx
@@ -1,4 +1,6 @@
-const getWindowDimensions = () => {
+import { useEffect, useState, useCallback } from 'react';
+
+export const getWindowDimensions = () => {
     const { innerWidth: width, innerHeight: height } = window;
     return {
         width,
@@ -6,4 +8,47 @@ const getWindowDimensions = () => {
     };
 };
 
-export default getWindowDimensions;
+export const useWindowDimensions = () => {
+    const initWindowDimensions = getWindowDimensions();
+    const [windowSize, setWindowSize] = useState(initWindowDimensions);
+
+    const resizeCallback = useCallback(() => {
+        setWindowSize(getWindowDimensions());
+    }, [setWindowSize]);
+
+    // checks to see if the window is being resized and sets the window state
+    useEffect(() => {
+        window.addEventListener('resize', resizeCallback);
+        return () => window.removeEventListener('resize', resizeCallback);
+    }, [resizeCallback]);
+
+    return windowSize;
+};
+
+export const useImage = fileName => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        const getImage = async () => {
+            try {
+                const res = await import(`../assets/${fileName}`);
+                setImage(res.default);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getImage();
+    }, [fileName]);
+
+    return {
+        loading,
+        error,
+        image,
+    };
+};
+
+// export default { getWindowDimensions, useImage };
