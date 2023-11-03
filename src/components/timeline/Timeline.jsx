dbfb43dd55fd0e0381215b5dd37ae1e79c0d92fe
Justin Kang <68215015+justinkang01@users.noreply.github.com>
2023-11-03 15:42:57 -0400

    first edits to timeline

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import styles from './timeline.module.scss';
 
+const data = ['test1', 'test2', 'test3'];
 const Timeline = () => {
-    return <div className={styles.timelineContainer}>Timeline</div>;
+    return (
+        <div className={styles.timelineContainer}>
+            <div className={styles.timelineWrapper}>
+                {data.map((elm, idx) => (
+                    <div className={styles.item} key={idx}>
+                        {elm}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Timeline;
