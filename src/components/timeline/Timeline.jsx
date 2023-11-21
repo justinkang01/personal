5891443408b2071ea2581c4d2c8faf26525cac68
Justin Kang <68215015+justinkang01@users.noreply.github.com>
2023-11-21 15:49:02 -0500

    no magic numbers

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -5,26 +5,34 @@ import styles from './timeline.module.scss';
 const data = ['2001', '2019', '2022', '2023'];
 const Timeline = () => {
     const windowDimen = useWindowDimensions();
-    const windowWidth = windowDimen.width;
+
     const START_YEAR = 2001;
     const END_YEAR = 2023;
+
     const zeroHorRef = useRef(null);
     const zeroYearRef = useRef(null);
     const containerRef = useRef(null);
+    const timelineRef = useRef(null);
+    const circleRef = useRef(null);
+
     const [horPadding, setHorPadding] = useState(0);
     const [yearPadding, setYearPadding] = useState(0);
     const [containerWidth, setContainerWidth] = useState(0);
+    const [timelineHeight, setTimelineHeight] = useState(0);
+    const [circleHeight, setCircleHeight] = useState(0);
 
     useEffect(() => {
         setHorPadding(zeroHorRef.current.offsetWidth);
         setYearPadding(zeroYearRef.current.offsetWidth);
         setContainerWidth(containerRef.current.offsetWidth);
-    }, [windowWidth]);
+        setTimelineHeight(timelineRef.current.offsetHeight);
+        setCircleHeight(circleRef.current.offsetHeight);
+    }, [windowDimen]);
 
     const find_style = (yr, idx) => {
         const yearsBetween = END_YEAR - START_YEAR;
-        const relPosPercentage = ((yr - START_YEAR) / yearsBetween) * 320; // mult by height of timeline
-        const vertical = relPosPercentage + 10;
+        const relPosPercentage = ((yr - START_YEAR) / yearsBetween) * timelineHeight; // mult by height of timeline
+        const vertical = relPosPercentage + circleHeight;
         const horizontal =
             idx % 2 === 0
                 ? { left: containerWidth / 2 - (horPadding + yearPadding), flexDirection: 'row-reverse' }
@@ -37,8 +45,8 @@ const Timeline = () => {
                 <h1>.timeline</h1>
                 <div className={styles.timeline} ref={containerRef}>
                     <div className={styles.crl0}></div>
-                    <div className={styles.vl}></div>
-                    <div className={styles.crl1}></div>
+                    <div className={styles.vl} ref={timelineRef}></div>
+                    <div className={styles.crl1} ref={circleRef}></div>
                     <div className={styles.crl2}></div>
                     <div className={styles.crl3}></div>
                     {data.map((elm, idx) => {
